57d063c2a0b07e8336c27a944b6809e4828410f6
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-05-07 20:05:12 +0530

    07-May-22: Sat, Added Session21Practice.java

---
+++ Session21Practice.java
@@ -0,0 +1,16 @@
+package com.practice.session21;
+
+public class Session21Practice {
+	
+	public static void main(String[] args) {
+		
+		Animal animal1 = new Animal();
+		animal1.noOfLegs = 4;
+	}
+}
+
+class Animal {
+		
+	public int noOfLegs;
+	boolean isCarnivore;
+}
