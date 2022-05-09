099afc1711036e7a104e64053018edec0efea0b9
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-05-09 20:44:12 +0530

    09-May-22:Mon, Added Session22Practice.java

---
+++ Session22Practice.java
@@ -0,0 +1,15 @@
+package com.practice.session22;
+
+public class Session22Practice {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("The numbers are: ");
+		
+		int i=1;
+		while(i <= 5) {
+			System.out.println(i);
+			i = i+1;
+		}
+	}
+}
