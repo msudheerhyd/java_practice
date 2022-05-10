e7c6af72708c26cffe9f79a292ea94cb991409cd
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-05-10 20:22:03 +0530

    10-May-22:Thurs, Added Session23Practice.java

---
+++ Session23Practice.java
@@ -0,0 +1,15 @@
+package com.practice.session23;
+
+import java.util.Scanner;
+
+public class Session23Practice {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter name: ");
+		
+		String name = sc.nextLine();
+		System.out.println("Your name is " + name);
+	}
+}
