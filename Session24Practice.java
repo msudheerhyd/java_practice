8b655927c2fa8eeaf28baf4b05b9a7492823354c
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-05-11 20:28:34 +0530

    11-May-22:Wed, Added Session24Practice.java

---
+++ Session24Practice.java
@@ -0,0 +1,13 @@
+package com.practice.session24;
+
+import com.practice.session23.Animal;
+
+public class Session24Practice {
+	
+	public static void main(String[] args) {
+		
+		Animal animal1 = new Animal("Lion", 4);
+		System.out.println(animal1);
+	}
+}
+
