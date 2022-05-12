2caaa1f3aa6090611777620db0332a9b7c983cab
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-05-12 20:10:52 +0530

    12-May-22:Thurs, Added Session25Practice.java

---
+++ Session25Practice.java
@@ -0,0 +1,15 @@
+package com.practice.session25;
+
+public class Session25Practice {
+	
+	public static void main(String[] args) {
+		
+		int number = 9;
+		double someDouble = number;
+		int someInt = (int) someDouble;
+		
+		System.out.println(number);
+		System.out.println(someDouble);
+		System.out.println(someInt);
+	}
+}
