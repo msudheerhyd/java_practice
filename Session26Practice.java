c4791ef300451ff50ac9da20df88f60397e78e75
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-05-13 20:09:15 +0530

    13-May-22:Fri, Added Session26Practice.java

---
+++ Session26Practice.java
@@ -0,0 +1,21 @@
+package com.practice.session26;
+
+public class Session26Practice {
+	
+	public static void main(String[] args) {
+		
+		int n = 7;
+		
+		switch (n) {
+			case 6: 
+				System.out.println("Today is Saturday");
+				break;	
+			case 7:
+				System.out.println("Today is Sunday");
+				break;
+				
+			default:
+				System.out.println("Waiting for weekend");
+		}
+	}
+}
