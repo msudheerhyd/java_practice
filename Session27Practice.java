899c43bc4bedd2ef4223f6c77f6c96105fab462a
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-05-14 20:17:06 +0530

    14-May-22:Sat, Added Session27Practice.java

---
+++ Session27Practice.java
@@ -0,0 +1,18 @@
+package com.practice.session27;
+
+public class Session27Practice {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++) {
+			if (i == 5) {
+				continue;
+			}
+			
+			System.out.println(i);
+			if (i == 8) {
+				break;
+			}
+		}
+	}
+}
