ed16cf976928f8322cd8a3a2c1bdea0f5301d8a2
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-05-17 20:27:35 +0530

    17-May-22:Tue, Added Session30Practiccjava and Driver.java-Youtube project

---
+++ Session30Practice.java
@@ -0,0 +1,33 @@
+package com.practice.session30;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Session30Practice {
+	
+	public static void main(String[] args) {
+		try {
+			File myObj = new File("sudheer.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+			myReader.close();
+		}	catch (FileNotFoundException e) {
+			System.out.println("An error occured.");
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
