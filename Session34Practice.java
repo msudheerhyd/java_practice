8145437be1ee1ea2d4b23ae1fcb0903992795dd3
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-05-21 20:28:59 +0530

    21-May-22:Sat, Added Session34Practice.java

---
+++ Session34Practice.java
@@ -0,0 +1,14 @@
+package com.practice.session34;
+
+public class Session34Practice {
+	
+	public static void main(String[] args) {
+		
+		int someInt = (int)(Math.random() * 201);
+		System.out.println(someInt);
+		System.out.println(Math.max(5, 10));
+		System.out.println(Math.min(5, 10));
+		System.out.println(Math.sqrt(64));
+		System.out.println(Math.abs(-4.7));	
+	}
+}
