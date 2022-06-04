49ea544df3827fc67ad396479390675f654ecb5a
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-06-04 19:35:43 +0530

    04-June-22:Sat, Added YoutubeSession16

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboottutorial/repository/DepartmentRepository.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboottutorial/repository/DepartmentRepository.java
@@ -2,10 +2,14 @@ package com.dailycodebuffer.Springboottutorial.repository;
 
 import com.dailycodebuffer.Springboottutorial.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
     public Department findByDepartmentName(String departmentName);
+
+    @Query(value = "", nativeQuery = true)
+    public Department findByDepartmentNameIgnoreCase(String departmentName);
 }
