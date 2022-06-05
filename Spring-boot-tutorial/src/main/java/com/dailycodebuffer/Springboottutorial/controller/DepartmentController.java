1da0a6ef5a26a0d6757e3396d8912941fd4b8b82
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-06-05 20:14:16 +0530

    05-June-22:Sun, Added Spring-boot tutorial

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentController.java
@@ -1,11 +1,16 @@
 package com.dailycodebuffer.Springboottutorial.controller;
 
 import com.dailycodebuffer.Springboottutorial.entity.Department;
+import com.dailycodebuffer.Springboottutorial.error.DepartmentNotFoundException;
 import com.dailycodebuffer.Springboottutorial.service.DepartmentService;
 import com.dailycodebuffer.Springboottutorial.service.DepartmentServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.Name;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -13,18 +18,22 @@ public class DepartmentController {
 
     @Autowired
     private DepartmentService departmentService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department) {
+    public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside savedepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
-    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) throws DepartmentNotFoundException {
         return departmentService.fetchDepartmentById(departmentId);
     }
 
--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboottutorial/service/DepartmentService.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboottutorial/service/DepartmentService.java
@@ -1,6 +1,7 @@
 package com.dailycodebuffer.Springboottutorial.service;
 
 import com.dailycodebuffer.Springboottutorial.entity.Department;
+import com.dailycodebuffer.Springboottutorial.error.DepartmentNotFoundException;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ public interface DepartmentService {
 
     public List<Department> fetchDepartmentList();
 
-    public Department fetchDepartmentById(Long departmentId);
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
 
     public void deleteDepartmentById(Long departmentId);
 
