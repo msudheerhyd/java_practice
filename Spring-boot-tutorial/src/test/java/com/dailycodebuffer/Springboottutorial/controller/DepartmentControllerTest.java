f8696165c06fffb0bd89598bc90e9ee9a4c9069c
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-06-12 17:39:13 +0530

    12-June-22:,Sun Added SpringBoot Tutorial

--- Spring-boot-tutorial/src/test/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentControllerTest.java
+++ Spring-boot-tutorial/src/test/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentControllerTest.java
@@ -1,6 +1,7 @@
 package com.dailycodebuffer.Springboottutorial.controller;
 
 import com.dailycodebuffer.Springboottutorial.entity.Department;
+import com.dailycodebuffer.Springboottutorial.error.DepartmentNotFoundException;
 import com.dailycodebuffer.Springboottutorial.service.DepartmentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -60,6 +61,14 @@ class DepartmentControllerTest {
     }
 
     @Test
-    void fetchDepartmentById() {
+    void fetchDepartmentById() throws Exception {
+        Mockito.when(departmentService.fetchDepartmentById(1L))
+                .thenReturn(department);
+
+        mockMvc.perform(get("/departments/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.departmentName").
+                        value(department.getDepartmentName()));
     }
 }
