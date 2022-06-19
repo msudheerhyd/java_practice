a07695d05def13e6ac0ee71de2ae69d34a4be107
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-06-19 21:47:13 +0530

    19-June-22:Sun, Added YoutubeSession25 and EmployeeSystemApiApplication.java

---
+++ youtubeSession25/Driver.java
@@ -0,0 +1,9 @@
+package com.practice.youtubeSession25;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		Game g = new Game();
+		g.start();
+	}
+}
---
+++ youtubeSession25/Game.java
@@ -0,0 +1,50 @@
+package com.practice.youtubeSession25;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+	private ArrayList<Question> questionSet;
+	public Game() {
+		 questionSet = new ArrayList<Question>();
+		 String q = "Where does the Sun rise?";
+		 String[] a = {"East", "South", "West", "North"};
+		 questionSet.add(new Question(q, a, "East"));
+		 q = "Who invented the telephone?";
+		 //must reuse variable wit new String[]
+		 //array constants can only be used during initialization
+		 a = new String[] {"Thomas Edison", "Michael Faraday", "James Watt",
+				 "Alexander Bell"};
+		 questionSet.add(new Question(q, a, "Alexander Bell"));
+		 q = "Which is the capital of Japan?";
+		 a = new String[]{"Beijing", "Tokyo", "Seoul", "Bangkok"};
+		 questionSet.add(new Question(q, a, "Tokyo"));
+		 Collections.shuffle(questionSet, new Random()); 
+	}
+	public void start() {
+		Scanner scan = new Scanner(System.in);
+		int numCorrect = 0;
+		// show questions from questionSet
+		for (int question = 0; question < questionSet.size(); question++) {
+			System.out.println(questionSet.get(question).getQuestion());
+			int numChoices = questionSet.get(question).getChoices().size();
+			// show choices from questions in questionSet
+			for (int choice = 0; choice < numChoices; choice++) {
+				System.out.println((choice + 1) + ": " + 
+						questionSet.get(question).getChoices().get(choice));
+			}
+			int playerAnswer = scan.nextInt();
+				ArrayList<String> choiceSet = 
+						questionSet.get(question).getChoices();
+				String correctAnswer = questionSet.get(question).getAnswer();
+				int correctAnswerIndex = choiceSet.indexOf(correctAnswer);
+				if (playerAnswer == correctAnswerIndex + 1) {
+					numCorrect++;
+				}
+		}
+		scan.close();
+		System.out.println("You got " + numCorrect + " correct answer(s)");
+	}
+}
