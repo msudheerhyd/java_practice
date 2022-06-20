b098a3c18b9fc66918147fcb70462011fc6088ac
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-06-20 19:29:29 +0530

    20-June-22:Mon, Added Employee-Management-System-ui

---
+++ employee-management-system-ui/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AddEmployee from './components/AddEmployee';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+   <Navbar />
+   <AddEmployee/>
+   </>
+  );
+}
+
+export default App;
