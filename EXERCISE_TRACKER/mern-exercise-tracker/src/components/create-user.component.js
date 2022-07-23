cc77fe985fae1d132c904d1481beb68d91d78b5b
Sudheer Mocharla <msudheerhyd@gmail.com>
2022-07-23 21:54:09 +0530

    23-July-22:Sat, Added Exercise_tracker

--- EXERCISE_TRACKER/mern-exercise-tracker/src/components/create-user.component.js
+++ EXERCISE_TRACKER/mern-exercise-tracker/src/components/create-user.component.js
@@ -1,6 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUsers extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    
+        this.state = {
+          username: '',
+        }
+      }
+
+      onChangeUsername(e) {
+        this.setState({
+          username: e.target.value
+        })
+      }
+    
+
+      onSubmit(e) {
+        e.preventDefault();
+    
+        const user = {
+          username: this.state.username,
+        }
+    
+        console.log(user);
+
+        axios.post('https://localhost:5000/users/add', user)
+          .then(res => console.log(res.data));
+
+        this.setState({
+            username: ''
+        })
+    
+      }
     render() {
         return (
             <div>
